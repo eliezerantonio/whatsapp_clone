def25bf15df84a544fff9bf447b5a24bfd821a14
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-07-07 00:43:39 +0100

    logar e carregar usuairo logado

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,12 +1,26 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:whatsap_clone/models/usuario.dart';
+import 'package:whatsap_clone/screens/home_screen.dart';
 
 import 'cadastro_screen.dart';
 
 const LOGIN_SCREEN = "/login_screen";
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({Key key}) : super(key: key);
 
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+final _controllerEmail = new TextEditingController();
+
+final _controllerSenha = new TextEditingController();
+
+String _mensagemErro = "";
+
+class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,6 +42,7 @@ class LoginScreen extends StatelessWidget {
                   height: 32,
                 ),
                 TextField(
+                  controller: _controllerEmail,
                   autofocus: true,
                   keyboardType: TextInputType.emailAddress,
                   style: TextStyle(fontSize: 20),
@@ -43,6 +58,7 @@ class LoginScreen extends StatelessWidget {
                 ),
                 SizedBox(height: 10),
                 TextField(
+                  controller: _controllerSenha,
                   obscureText: true,
                   style: TextStyle(fontSize: 20),
                   decoration: InputDecoration(
@@ -59,7 +75,7 @@ class LoginScreen extends StatelessWidget {
                 RaisedButton(
                   shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(20)),
-                  onPressed: () {},
+                  onPressed: _logarUsuario,
                   child: Text(
                     "Entrar",
                     style: TextStyle(
@@ -79,11 +95,79 @@ class LoginScreen extends StatelessWidget {
                     style: TextStyle(color: Colors.white),
                   ),
                 ),
+                Center(
+                  child: Text(_mensagemErro,
+                      style: TextStyle(color: Colors.red, fontSize: 20)),
+                )
               ],
             ),
           ),
         ),
       ),
     );
+  }
+
+  void _logarUsuario() {
+    //recuperar dados dos campos
+
+    String email = _controllerEmail.text;
+    String senha = _controllerSenha.text;
+
+    if (email.isNotEmpty && email.contains("@")) {
+      if (senha.trim().length > 5) {
+        setState(() {
+          _mensagemErro = "";
+        });
+        // executar metodo
+
+        Usuario usuario = Usuario();
+
+        usuario.senha = senha;
+        usuario.email = email;
+
+        _cadastrarUsuario(usuario);
+      } else {
+        setState(() {
+          _mensagemErro = "Preecnha a senha";
+        });
+      }
+    } else {
+      setState(() {
+        _mensagemErro = "Preencha o email correto";
+      });
+    }
+  }
+
+  _cadastrarUsuario(Usuario usuairo) {
+    FirebaseAuth auth = FirebaseAuth.instance;
+
+    auth
+        .signInWithEmailAndPassword(
+      email: usuairo.email,
+      password: usuairo.senha,
+    )
+        .then((firebaseUser) {
+      Navigator.pushReplacementNamed(context, HOME_SCREEN);
+    }).catchError((onError) {
+      setState(() {
+        _mensagemErro = "Erro ao fazer logineli";
+      });
+    });
+  }
+
+  Future _verificaUsuarioLogado() async {
+    FirebaseAuth auth = FirebaseAuth.instance;
+
+    FirebaseUser usuarioLogado = await auth.currentUser();
+
+    if (usuarioLogado != null) {
+      Navigator.pushReplacementNamed(context, HOME_SCREEN);
+    }
+  }
+
+  @override
+  void initState() {
+    _verificaUsuarioLogado();
+    super.initState();
   }
 }
