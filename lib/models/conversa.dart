26edf063e1f797f5205d7beff3affaa6a7b6e4a2
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-07-08 00:57:48 +0100

    listando conversas

--- lib/models/conversa.dart
+++ lib/models/conversa.dart
@@ -2,15 +2,17 @@ class Conversa {
   String _nome;
   String _mensagem;
   String _caminhoFoto;
- String get nome => this._nome;
 
- set nome(String value) => this._nome = value;
+  Conversa([this._nome, this._mensagem, this._caminhoFoto]);
+  String get nome => this._nome;
+
+  set nome(String value) => this._nome = value;
 
   get mensagem => this._mensagem;
 
- set mensagem( value) => this._mensagem = value;
+  set mensagem(value) => this._mensagem = value;
 
   get caminhoFoto => this._caminhoFoto;
 
- set caminhoFoto( value) => this._caminhoFoto = value;
+  set caminhoFoto(value) => this._caminhoFoto = value;
 }
