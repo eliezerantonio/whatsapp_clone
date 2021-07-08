e213bf8744f2ece6815dc288e7a40bf317a6d6f1
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-07-08 19:54:22 +0100

    carregando imagem do user

--- lib/screens/configuracoes_screen.dart
+++ lib/screens/configuracoes_screen.dart
@@ -1,5 +1,7 @@
 import 'dart:io';
 
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 
@@ -15,6 +17,10 @@ class ConfiguracoesScreen extends StatefulWidget {
 class _ConfiguracoesScreenState extends State<ConfiguracoesScreen> {
   final _controllerNome = TextEditingController();
   File _imagem;
+  bool _subindoImagem = false;
+  String _urlImageRecuperada;
+
+  String _idUsuarioLogado;
   Future _recuperarImagem(String origemImage) async {
     PickedFile imagemSelecionada;
     switch (origemImage) {
@@ -30,7 +36,61 @@ class _ConfiguracoesScreenState extends State<ConfiguracoesScreen> {
 
     setState(() {
       _imagem = File(imagemSelecionada.path);
+      if (_imagem != null) {
+        _subindoImagem = true;
+        _uploadImagem();
+      }
     });
+  }
+
+  Future _uploadImagem() async {
+    FirebaseStorage storage = FirebaseStorage.instance;
+
+    StorageReference pastaRaiz = storage.ref();
+
+    StorageReference arquivo =
+        pastaRaiz.child("perfil").child("$_idUsuarioLogado.jpg");
+    StorageUploadTask task = arquivo.putFile(_imagem);
+
+    task.events.listen((event) {
+      if (event.type == StorageTaskEventType.progress) {
+        setState(() {
+          _subindoImagem = true;
+        });
+      } else if (event.type == StorageTaskEventType.success) {
+        setState(() {
+          _subindoImagem = false;
+        });
+      }
+    });
+
+    //recuperar URl da Imagem
+
+    task.onComplete.then((snaphot) {
+      _recuperarUrlImagem(snaphot);
+    });
+  }
+
+  Future _recuperarUrlImagem(StorageTaskSnapshot snapshot) async {
+    String url = await snapshot.ref.getDownloadURL();
+
+    setState(() {
+      _urlImageRecuperada = url;
+    });
+  }
+
+  _recuperarDadosUsuario() async {
+    FirebaseAuth auth = FirebaseAuth.instance;
+
+    FirebaseUser usuarioLogado = await auth.currentUser();
+
+    _idUsuarioLogado = usuarioLogado.uid;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _recuperarDadosUsuario();
   }
 
   @override
@@ -45,12 +105,13 @@ class _ConfiguracoesScreenState extends State<ConfiguracoesScreen> {
           child: SingleChildScrollView(
             child: Column(
               children: [
-                //Carregando
+                _subindoImagem ? Text("Carregando") : Container(),
                 CircleAvatar(
                   radius: 100,
                   backgroundColor: Colors.grey,
-                  backgroundImage: NetworkImage(
-                      "https://firebasestorage.googleapis.com/v0/b/whatsapp-f57f1.appspot.com/o/perfil%2Fperfil6.jpeg?alt=media&token=2ff42684-f4bf-4547-8d15-0a45ff384cc0"),
+                  backgroundImage: NetworkImage(_urlImageRecuperada == null
+                      ? "https://firebasestorage.googleapis.com/v0/b/whatsapp-f57f1.appspot.com/o/perfil%2Fperfil6.jpeg?alt=media&token=2ff42684-f4bf-4547-8d15-0a45ff384cc0"
+                      : _urlImageRecuperada),
                 ),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
