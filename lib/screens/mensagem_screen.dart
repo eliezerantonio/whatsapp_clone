ea477e1b9263cbb794ff7da04d8db24452711f5a
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-07-09 02:28:08 +0100

    tela de mesnagem (caixa de mesnagem)

--- lib/screens/mensagem_screen.dart
+++ lib/screens/mensagem_screen.dart
@@ -12,13 +12,68 @@ class MensagemScreen extends StatefulWidget {
 }
 
 class _MensagemScreenState extends State<MensagemScreen> {
+  final _controllerMensagem = TextEditingController();
   @override
   Widget build(BuildContext context) {
+    var caixaMensagem = Container(
+        padding: EdgeInsets.all(8),
+        child: Row(
+          children: [
+            Expanded(
+              child: Padding(
+                padding: EdgeInsets.only(
+                  right: 8,
+                ),
+                child: TextField(
+                  style: TextStyle(fontSize: 20),
+                  autofocus: true,
+                  controller: _controllerMensagem,
+                  decoration: InputDecoration(
+                      contentPadding: EdgeInsets.fromLTRB(32, 16, 32, 16),
+                      hintText: "Digite uma mensagem...",
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      prefixIcon: IconButton(
+                          icon: Icon(Icons.camera_alt),
+                          onPressed: _enviarFoto)),
+                ),
+              ),
+            ),
+            FloatingActionButton(
+              onPressed: () {},
+              mini: true,
+              child: Icon(Icons.send, color: Colors.white),
+              backgroundColor: Theme.of(context).primaryColor,
+            )
+          ],
+        ));
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.contato.nome),
       ),
-      body: Container(),
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage("assets/bg.png"), fit: BoxFit.cover),
+        ),
+        child: SafeArea(
+          child: Container(
+            padding: EdgeInsets.all(16),
+            child: Column(
+              children: [
+                Text("ListView"),
+                caixaMensagem,
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
+
+  void _enviarFoto() {}
 }
